79057ac2d1bf7cdedcab87aa03eb7d5da9837159
tri <tribestst123@gmail.com>
2024-08-07 21:20:40 +0700

    update

--- src/socket/index.js
+++ src/socket/index.js
@@ -1,6 +1,8 @@
 import { io } from "socket.io-client";
 
-const URL = "http://localhost:4000";
+// const URL = "http://localhost:4000";
+const URL = "https://capstone-project-backend-q6xd.onrender.com";
+
 export const socket = io(URL, {
   autoConnect: false,
 });
