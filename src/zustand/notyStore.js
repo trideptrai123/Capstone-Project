d916b1d39d51524e06b54eb09eedc0963a9a4b6f
tri <tribestst123@gmail.com>
2024-08-12 23:17:11 +0700

    fix

--- src/zustand/notyStore.js
+++ src/zustand/notyStore.js
@@ -1,5 +1,5 @@
-import {create} from 'zustand';
-import { persist } from 'zustand/middleware';
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 const useNotyStore = create(
   persist(
@@ -7,22 +7,25 @@ const useNotyStore = create(
       notifications: [],
       addNotification: (notification) =>
         set((state) => ({
-          notifications: [notification,...state.notifications],
+          notifications: state?.notifications?.find(
+            (i) => i?._id == notification?._id
+          )
+            ? [...state.notifications]
+            : [notification, ...state.notifications],
         })),
       setNotification: (list) =>
-          set((state) => ({
-            notifications: [...list],
-          })),
+        set((state) => ({
+          notifications: [...list],
+        })),
       removeNotification: (id) =>
         set((state) => ({
           notifications: state.notifications.filter((n) => n.id !== id),
         })),
     }),
     {
-      name: 'notifications-storage',
+      name: "notifications-storage",
     }
   )
 );
 
 export default useNotyStore;
-
